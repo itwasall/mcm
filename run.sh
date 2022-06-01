60fd4d98fdba2b387528ee39dbb5da1031a1490d
refrshrs <48458647+refrshrs@users.noreply.github.com>
2022-06-01 14:33:06 +0100

    keypress

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count"
 ioreg -c IOPCIDevice -rn "GFX0" | grep "model"
 sysctl -n machdep.cpu.brand_string
@@ -10,5 +10,7 @@ read -t 3 -n 1
 if [$? = 0 ] ; then
 diskutil erasedisk APFS "Macintosh HD" /dev/disk0
 exit ;
+else
+echo "waiting for keypresss"
 fi 
 done
