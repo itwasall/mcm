639ccb1d05016cf1ff29c82bb3dfae6b4ed09657
refrshrs <48458647+refrshrs@users.noreply.github.com>
2022-06-01 15:17:59 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,5 @@ ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count"
 ioreg -c IOPCIDevice -rn "GFX0" | grep "model"
 sysctl -n machdep.cpu.brand_string
 sysctl hw.memsize
-
-echo "Press key to format disk"
-while [true] ; do
-read -t 3 -n 1
-if [$? = 0 ] ; then
+diskutil list disk0
 diskutil erasedisk APFS "Macintosh HD" /dev/disk0
-exit ;
-else
-echo "waiting for keypresss"
-fi 
-done
