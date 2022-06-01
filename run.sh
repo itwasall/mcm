b071ec3d205ee53acb66c62be669733430ccd1fc
refrshrs <48458647+refrshrs@users.noreply.github.com>
2022-06-01 14:26:17 +0100

    fixed it

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Capacity"
+ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count"
 ioreg -c IOPCIDevice -rn "GFX0" | grep "model"
 sysctl -n machdep.cpu.brand_string
 sysctl hw.memsize
+
