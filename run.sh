966ee047e9842dc3d4d2f7135011c5aa58a8ed51
refrshrs <nthorgan@live.co.uk>
2022-06-06 22:44:22 +0100

    Nice

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "####### CYCLE COUNT #######"
-ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count"
+ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count" | awk '{i=0;while($i !~ /Cycle Count/) i++; print $i}'
 echo "####### GPU #######"
 ioreg -c IOPCIDevice -rn "GFX0" | grep "model"
 echo "####### CPU #######"
 sysctl -n machdep.cpu.brand_string
 echo "####### RAM #######"
-sysctl hw.memsize
+sysctl hw.memsize | awk '{ mem_bytes=0; print mem_bytes/1024/1024/1024, "GB" }'
 echo "####### HDD #######"
 diskutil list disk0
 diskutil quiet erasedisk APFS "Macintosh HD" /dev/disk0
