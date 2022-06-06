8267c72c25b5a3b6601be3e84692a859f331623c
refrshrs <48458647+refrshrs@users.noreply.github.com>
2022-06-06 11:19:00 +0100

    Updated run.sh
    Added notes to separate commands, put diskutil on silent

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+echo "####### CYCLE COUNT #######"
 ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count"
+echo "####### GPU #######"
 ioreg -c IOPCIDevice -rn "GFX0" | grep "model"
+echo "####### CPU #######"
 sysctl -n machdep.cpu.brand_string
+echo "####### RAM #######"
 sysctl hw.memsize
+echo "####### HDD #######"
 diskutil list disk0
 diskutil erasedisk APFS "Macintosh HD" /dev/disk0
+echo "####### HDD FORMATTED #######"
+echo "####### SERIAL NUMBER #######"
 ioreg -c IOPlatformExpertDevice -r | grep "IOPlatformSerialNumber"
