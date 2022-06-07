a5835ea3eee9e8f5f022e80c64fd135304334804
refrshrs <48458647+refrshrs@users.noreply.github.com>
2022-06-07 10:30:44 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ ioreg -c IOPCIDevice -rn "GFX0" | grep "model"
 echo "####### CPU #######"
 sysctl -n machdep.cpu.brand_string
 echo "####### RAM #######"
-sysctl hw.memsize | awk '{ mem_bytes=0; print mem_bytes/1024/1024/1024, "GB" }'
+sysctl hw.memsize | awk '{print $2/1024/1024/1024, "GB" }'
 echo "####### HDD #######"
 diskutil list disk0
 diskutil quiet erasedisk APFS "Macintosh HD" /dev/disk0
