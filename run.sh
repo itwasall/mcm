7ce39c5bd31d770955987f01036db555c4dd44fc
refrshrs <48458647+refrshrs@users.noreply.github.com>
2022-06-08 10:09:12 +0100

    Update run.sh
    added bat argument

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "####### CYCLE COUNT #######"
-ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count"
-echo "####### MAX CYCLE COUNT #######"
-ioreg -c AppleSmartBattery | grep "DesignCycleCount"
-echo "####### BATTERY HEALTH #######"
-ioreg -c IOACPIPlatformDevice -rln "SMB0" | awk '{{if ($1 ~ /MaxCapacity/) maxcap=$3; else if ($1 ~ /DesignCapacity/) descap=$3} {if (maxcap != 0) {if (descap != 0) print (maxcap*100)/descap}}}' | grep -m 1 .
+if $1 == "bat" then
+  echo "####### CYCLE COUNT #######"
+  ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count"
+  echo "####### MAX CYCLE COUNT #######"
+  ioreg -c AppleSmartBattery | grep "DesignCycleCount"
+  echo "####### BATTERY HEALTH #######"
+  ioreg -c IOACPIPlatformDevice -rln "SMB0" | awk '{{if ($1 ~ /MaxCapacity/) maxcap=$3; else if ($1 ~ /DesignCapacity/) descap=$3} {if (maxcap != 0) {if (descap != 0) print (maxcap*100)/descap}}}' | grep -m 1 .
+fi
 echo "####### GPU #######"
 ioreg -c IOPCIDevice -rn "GFX0" | grep "model"
 echo "####### CPU #######"
