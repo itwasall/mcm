ff8e1e32a49161aa48898ccfea78bc022d2084d8
refrshrs <48458647+refrshrs@users.noreply.github.com>
2022-06-08 10:13:10 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [[$1 = "bat"]] then
+if [[$1 = "bat"]] 
+then
   echo "####### CYCLE COUNT #######"
   ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count"
   echo "####### MAX CYCLE COUNT #######"
