03faa1ebfb1d0ad3f1c6368307aaee5dcd5cc10c
refrshrs <48458647+refrshrs@users.noreply.github.com>
2022-10-13 16:21:27 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ diskutil list disk0
 echo "####### SERIAL NUMBER #######"
 ioreg -c IOPlatformExpertDevice -r | grep "IOPlatformSerialNumber"
 
-while getops m:b:c flag
+while getopts m:b:c: flag
 do 
   case "${flag}" in
     m) /Install\ macOS\ Monterey.app/Contents/Resources/startosinstall --agreetolicense --volume /Volumes/Macintosh\ HD;;
