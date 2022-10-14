1c725bcdc203c88cfec0da837172fd1cea174ad7
refrshrs <48458647+refrshrs@users.noreply.github.com>
2022-10-14 09:26:55 +0100

    Update run.sh
    hopefully now it's more fully featured

--- run.sh
+++ run.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
-echo "####### CYCLE COUNT #######"
-ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count"
-echo "####### MAX CYCLE COUNT #######"
-ioreg -c AppleSmartBattery | grep "DesignCycleCount"
-echo "####### BATTERY HEALTH #######"
-ioreg -c IOACPIPlatformDevice -rln "SMB0" | awk '{{if ($1 ~ /MaxCapacity/) maxcap=$3; else if ($1 ~ /DesignCapacity/) descap=$3} {if (maxcap != 0) {if (descap != 0) print (maxcap*100)/descap}}}' | grep -m 1 .
-echo "####### GPU #######"
-ioreg -c IOPCIDevice -w0 -rn "GFX0" | grep -E "model|totalMB"
-echo "####### CPU #######"
-sysctl -n machdep.cpu.brand_string
-echo "####### RAM #######"
-sysctl hw.memsize | awk '{print $2/1024/1024/1024, "GB" }'
-echo "####### HDD #######"
-diskutil list disk0
-echo "####### SERIAL NUMBER #######"
-ioreg -c IOPlatformExpertDevice -r | grep "IOPlatformSerialNumber"
 
 option_argument=$1
-do 
-  case $option_argument in
-    m) /Install\ macOS\ Monterey.app/Contents/Resources/startosinstall --agreetolicense --volume /Volumes/Macintosh\ HD;;
-    b) /Install\ macOS\ Big\ Sur.app/Contents/Resources/startosinstall --agreetolicense --volume /Volumes/Macintosh\ HD;;
-    c) /Install\ macOS\ Catalina.app/Contents/Resources/startosinstall --aggretolicense --volume /Volumes/Macintosh\ HD;;
+ case $option_argument in
+    i) echo "####### CYCLE COUNT #######"
+       ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count"
+       echo "####### MAX CYCLE COUNT #######"
+       ioreg -c AppleSmartBattery | grep "DesignCycleCount"
+       echo "####### BATTERY HEALTH #######"
+       ioreg -c IOACPIPlatformDevice -rln "SMB0" | awk '{{if ($1 ~ /MaxCapacity/) maxcap=$3; else if ($1 ~ /DesignCapacity/) descap=$3} {if (maxcap != 0) {if (descap != 0) print (maxcap*100)/descap}}}' | grep -m 1 .
+       echo "####### GPU #######"
+       ioreg -c IOPCIDevice -w0 -rn "GFX0" | grep -E "model|totalMB"
+       echo "####### CPU #######"
+       sysctl -n machdep.cpu.brand_string
+       echo "####### RAM #######"
+       sysctl hw.memsize | awk '{print $2/1024/1024/1024, "GB" }'
+       echo "####### HDD #######"
+       diskutil list disk0
+       echo "####### SERIAL NUMBER #######"
+       ioreg -c IOPlatformExpertDevice -r | grep "IOPlatformSerialNumber"
+       echo;;
+    ff) diskutil resetfusion;;
+    fa) diskutil erasedisk APFS "Macintosh HD" /dev/disk0
+    fj) diskutil erasedisk JFHS+ "Macintosh HD" /dev/disk0
+    im) /Install\ macOS\ Monterey.app/Contents/Resources/startosinstall --agreetolicense --volume /Volumes/Macintosh\ HD
+       echo;;
+    ib) /Install\ macOS\ Big\ Sur.app/Contents/Resources/startosinstall --agreetolicense --volume /Volumes/Macintosh\ HD
+       echo;;
+    ic) /Install\ macOS\ Catalina.app/Contents/Resources/startosinstall --aggretolicense --volume /Volumes/Macintosh\ HD
+       echo;;
+    ie) /Install\ macOS\ El\ Captiain.app/Contents/Resources/startosinstall --volume /Volumes/Macintosh\ HD --applicationpath /Install\ macOS\ El\ Captain.app
+       echo;;
   esac
 done
