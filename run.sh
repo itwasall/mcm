14ccad0948f186bb54f0074e33d58550289d489b
refrshrs <48458647+refrshrs@users.noreply.github.com>
2022-10-14 10:36:10 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,7 @@
 
 option_argument=$1
  case $option_argument in
-    i) echo "####### CYCLE COUNT #######"
-       ioreg -c IOACPIPlatformDevice -rln "SMB0" | grep "Cycle Count"
-       echo "####### MAX CYCLE COUNT #######"
-       ioreg -c AppleSmartBattery | grep "DesignCycleCount"
-       echo "####### BATTERY HEALTH #######"
-       ioreg -c IOACPIPlatformDevice -rln "SMB0" | awk '{{if ($1 ~ /MaxCapacity/) maxcap=$3; else if ($1 ~ /DesignCapacity/) descap=$3} {if (maxcap != 0) {if (descap != 0) print (maxcap*100)/descap}}}' | grep -m 1 .
+    i) 
        echo "####### GPU #######"
        ioreg -c IOPCIDevice -w0 -rn "GFX0" | grep -E "model|totalMB"
        echo "####### CPU #######"
@@ -15,13 +10,13 @@ option_argument=$1
        echo "####### RAM #######"
        sysctl hw.memsize | awk '{print $2/1024/1024/1024, "GB" }'
        echo "####### HDD #######"
-       diskutil list disk0
+       diskutil list /dev/disk0 | awk '{print $2 " GB"}' | sed -n '3 p'
        echo "####### SERIAL NUMBER #######"
        ioreg -c IOPlatformExpertDevice -r | grep "IOPlatformSerialNumber"
        echo;;
     r) diskutil resetfusion;;
-    a) diskutil erasedisk APFS "Macintosh HD" /dev/disk0
-    j) diskutil erasedisk JFHS+ "Macintosh HD" /dev/disk0
+    a) diskutil erasedisk APFS "Macintosh HD" /dev/disk0;;
+    j) diskutil erasedisk JFHS+ "Macintosh HD" /dev/disk0;;
     m) /Install\ macOS\ Monterey.app/Contents/Resources/startosinstall --agreetolicense --volume /Volumes/Macintosh\ HD
        echo;;
     b) /Install\ macOS\ Big\ Sur.app/Contents/Resources/startosinstall --agreetolicense --volume /Volumes/Macintosh\ HD
